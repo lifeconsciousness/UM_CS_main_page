db25b64a40d80fce5b62e827e0473a541a644530
lifeconsciousness <lifeconsciousness321@gmail.com>
2022-12-02 16:58:35 +0100

    image dissapearing/appearing on scroll

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 
 const backgroundImage = document.querySelector(".background-picture");
 const animationStartingPoint = 130;
+let firstTime = true
 
 document.addEventListener("scroll", ()=>{
     let scrollTop = window.pageXOffset || document.documentElement.scrollTop;
 
     if (scrollTop > animationStartingPoint){
+        backgroundImage.classList.remove("background-picture-animation-reverse")
         backgroundImage.classList.add("background-picture-animation")
-    } else if (scrollTop < animationStartingPoint){
-        
+        firstTime = false
+    } else{
+        if(!firstTime){
+            backgroundImage.classList.remove("background-picture-animation")
+            backgroundImage.classList.add("background-picture-animation-reverse")
+            
+        }
     }
+
 })
